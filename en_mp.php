1ab4163653c84bd6a0da9157c47de8c794dc1cac
Veela <veela.tseng@hyweb.com.tw>
2020-06-11 14:20:06 +0800

    新增三隻英文版css

--- en_mp.php
+++ en_mp.php
@@ -13,7 +13,7 @@
   <link rel="stylesheet" type="text/css" href="vendor/slick/slick.css" />
   <link rel="stylesheet" type="text/css" href="vendor/slick/slick-theme.css" />
   <!-- hyUI css -->
-  <link rel="stylesheet" href="css/cbc_en.css">
+  <link rel="stylesheet" href="css/cbc_en.css" id="cssStyle">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/PgwSlider/2.3.1/pgwslider.css">
 
 </head>
